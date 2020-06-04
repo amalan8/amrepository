e7c832fb9519b6c112197949dae5f66b82e7b5b6
am <firefly3051@outlook.com>
2020-06-04 14:49:51 -0700

    Homework week9 A. Malander

---
+++ Homework09BirthdayCardAMalander/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework09BirthdayCardAMalander.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Models.EnterCard cardInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SuccessForm", cardInfo);
+
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult SuccessForm()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Homework09BirthdayCardAMalander/Models/EnterCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework09BirthdayCardAMalander.Models
+{
+    public class EnterCard
+    {
+        [Required(ErrorMessage ="Please enter From")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+ 
+    }
+}
